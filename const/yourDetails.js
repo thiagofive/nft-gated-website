acae3369ff8ba7fe91b721520743c1c4e0489b7b
thiagofive <65563172+thiagofive@users.noreply.github.com>
2022-09-20 00:16:40 -0300

    Bug Fix

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -2,4 +2,4 @@
 export const contractAddress = "0x1A2F1965013d909C8539821CA7b1Bd4D616F09f9";
 
 // Replace this with your domain name.
-export const domainName = "example.com";
+export const domainName = "facebook.com";
